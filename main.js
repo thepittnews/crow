6491398978f05512ca473633fadd8d19c99c6c3f
Jon Moss <me@jonathanmoss.me>
2019-02-08 17:27:23 -0500

    initial MVP

---
+++ main.js
@@ -0,0 +1,169 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { existsSync } = require('fs');
+const { execSync } = require('child_process');
+const Client = require('ftp');
+const request = require('request');
+
+const config = require('./config');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({ width: 800, height: 600 });
+  win.loadFile('index.html');
+  win.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow();
+  }
+});
+
+function pdftk(command) {
+  return execSync(`pdftk ${command}`, { env: { PATH: '/usr/local/bin/:/usr/bin/' } }).toString();
+};
+
+function getPdfPath(dateSerialized, pageNumber) {
+  const [year, month, day] = dateSerialized.split('-');
+  var page = '';
+
+  if (pageNumber) {
+    page = `.${pageNumber}`
+  }
+
+  return `${config.path}/${month}-${day}-${year.substr(-2)}.PN_A${page}.pdf`;
+}
+
+function checkPages(args, sendClientAlert) {
+  const { dateSerialized, dateText } = args;
+
+  const pdfPath = getPdfPath(dateSerialized);
+  const pdfExists = existsSync(pdfPath);
+
+  if (pdfExists) {
+    sendClientAlert({ needsConfirmation: true, dateSerialized, dateText, pdfPath, taskName: `Date: ${dateText}\nExported PDF exists -- are you ready to SEND?` });
+  } else {
+    sendClientAlert({ taskName: `Exported PDF exists at ${pdfPath}`, status: 'fail' });
+  }
+};
+
+function sendPages(args, sendClientAlert) {
+  // ready to get started!
+  sendClientAlert({ taskName: 'Exported PDF exists', status: 'success' });
+
+  // get number of pages
+  sendClientAlert({ taskName: `Preparing to read PDF`, status: 'pending' });
+  try {
+    var pageNumbers = pdftk(`${args.pdfPath} dump_data`).split("\n").filter((line) => { return line.includes('NumberOfPages'); });
+    pageNumbers = Number(pageNumbers[0].split('NumberOfPages: ')[1]);
+    sendClientAlert({ taskName: `Preparing to read PDF`, status: 'success' });
+  } catch (e) {
+    sendClientAlert({ taskName: `Preparing to read PDF`, status: 'fail' });
+    sendClientAlert({ taskName: `ERROR: ${e}`, status: 'fail' });
+    return;
+  }
+
+  // split up PDFs up into individual PDFs
+  sendClientAlert({ taskName: `Preparing ${pageNumbers} pages`, status: 'pending' });
+
+  for(var i = 1; i <= pageNumbers; i++) {
+    try {
+      sendClientAlert({ taskName: `> Preparing page ${i}`, status: 'pending' });
+      pdftk(`${args.pdfPath} cat ${i}-${i} output ${getPdfPath(args.dateSerialized, i)}`);
+      sendClientAlert({ taskName: `> Preparing page ${i}`, status: 'success' });
+    } catch (e) {
+      sendClientAlert({ taskName: `> Preparing page ${i}`, status: 'fail' });
+      sendClientAlert({ taskName: `ERROR: ${e}`, status: 'fail' });
+      sendClientAlert({ taskName: `Preparing ${pageNumbers} pages`, status: 'fail' });
+      return;
+    }
+  }
+
+  sendClientAlert({ taskName: `Preparing ${pageNumbers} pages`, status: 'success' });
+
+  // send to printer servers
+  sendClientAlert({ taskName: `Sending ${pageNumbers} pages to printer`, status: 'pending' });
+
+  //var c = new Client();
+  //c.on('ready', function() {
+    for(var i = 1; i <= pageNumbers; i++) {
+      //var filename = getPdfPath(args.dateSerialized, i);
+      //c.put(filename, filename, function(e) {
+        //if (e) {
+          //sendClientAlert({ taskName: `> Sending page ${i}`, status: 'fail' });
+          //sendClientAlert({ taskName: `ERROR: ${e}`, status: 'fail' });
+          //sendClientAlert({ taskName: `Sending ${pageNumbers} pages to printer`, status: 'fail' });
+          //return;
+        //}
+
+        sendClientAlert({ taskName: `> Sending page ${i}`, status: 'success' });
+
+        if (i === pageNumbers) {
+          //c.end();
+        }
+      //});
+    }
+  //});
+
+  //c.connect({ host: '', port: '', user: '', password: '' });
+  //c.on('end', () => {
+    sendClientAlert({ taskName: `Sending ${pageNumbers} pages to printer`, status: 'success' });
+
+    // send notification to slack
+    request({
+      method: 'POST',
+      uri: config.slack_url,
+      form: {
+        payload: JSON.stringify({
+          channel: config.slack_channel,
+          icon_emoji: ':bird:',
+          text: `Pages sent for ${args.dateText}`,
+          username: 'crow'
+        })
+      }
+    }, (error, response, body) => {
+      console.log(response.statusCode);
+      console.log(body);
+
+      // all done
+      sendClientAlert({ taskName: 'SUCCESS', status: 'success' });
+    });
+  //});
+};
+
+ipcMain.on('send-pages', (event, args) => {
+  const sendClientAlert = (cbArg) => {
+    event.sender.send('send-pages-alert', cbArg);
+  };
+
+  if (args.confirmed === true) {
+    sendPages(args, sendClientAlert);
+  } else {
+    checkPages(args, sendClientAlert);
+  }
+});
---
+++ renderer.js
@@ -0,0 +1,92 @@
+const { ipcRenderer } = require("electron");
+
+const statusCssMap = {
+  'pending': 'warning',
+  'fail': 'danger',
+  'success': 'success'
+};
+
+function getConfirmation(arg) {
+  const { dateText, taskName } = arg;
+
+  const confirmed = confirm(taskName);
+  if (confirmed) {
+    document.getElementById('sendStatus').insertAdjacentHTML(
+      'afterbegin',
+      `<h3 style="text-align: center">Run Log for ${dateText}:</h3>`
+    );
+
+    ipcRenderer.send('send-pages', Object.assign({}, { confirmed: true }, arg));
+  } else {
+    Array.from(document.getElementsByClassName("sendButton"))
+    .filter((el) => { return el.disabled; })[0]
+    .disabled = false;
+  }
+};
+
+ipcRenderer.on('send-pages-alert', (event, arg) => {
+  const { dateSerialized, needsConfirmation, status, taskName } = arg;
+
+  if (needsConfirmation) {
+    return getConfirmation(arg);
+  }
+
+  const container = document.getElementById('sendStatus');
+  const taskId = taskName.split(/[^A-Za-z0-9]/).join("_");
+  const element = document.querySelector(`#sendStatus > #${taskId}`);
+  const cssClass = `alert alert-${statusCssMap[status]}`;
+
+  if (element) {
+    element.className = cssClass;
+  } else {
+    container.insertAdjacentHTML(
+      'beforeend',
+      `<div class="${cssClass}" role="alert" id="${taskId}">${taskName}</div>`
+    );
+  }
+
+  if ((taskName === 'SUCCESS' && status === 'success') || (status === 'fail'))  {
+    Array.from(document.getElementsByClassName("sendButton"))
+    .filter((el) => { return el.disabled; })[0]
+    .disabled = false;
+  }
+});
+
+setProductionDate();
+
+function setProductionDate() {
+  const now = new Date();
+  var date1;
+  var date2;
+
+  if (now.getHours() < 6) {
+    // We're already in publication day already.
+    date1 = new Date((new Date()).setDate(now.getDate() - 1));
+    date2 = now;
+  } else {
+    date1 = now;
+    date2 = new Date((new Date()).setDate(now.getDate() + 1));
+  }
+
+  const container = document.getElementsByClassName('list-group')[0];
+
+  [date1, date2].forEach((date) => {
+    const dateText = date.toLocaleString().split(",")[0];
+    const dateSerialized = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    const dateHTML = `<li class="list-group-item">${dateText}
+    <button style="float: right" type="button" class="btn btn-danger sendButton" data-date-serialized="${dateSerialized}" data-date-text=${dateText}>SEND!!!</button></li>`;
+
+    container.insertAdjacentHTML('beforeend', dateHTML);
+  });
+};
+
+Array.from(document.getElementsByClassName("sendButton")).forEach((el) => {
+  el.addEventListener("click", (e) => {
+    const el = e.toElement;
+    const { dateSerialized, dateText } = el.dataset;
+
+    el.disabled = true;
+    document.getElementById("sendStatus").innerHTML = '';
+    ipcRenderer.send('send-pages', { confirmed: false, dateSerialized, dateText });
+  });
+});
