77017b0dc23fb776ec7a67e357a2b8f91428c877
Jon Moss <me@jonathanmoss.me>
2019-07-23 20:23:00 -0400

    Send specific pages to printer

--- main.js
+++ main.js
@@ -40,9 +40,8 @@ app.on('activate', () => {
   if (win === null) createWindow();
 });
 
-const getPdfPath = (selectedFile, pageNumber) => {
-  return selectedFile.replace('PN_A.pdf', `PN_A.${pageNumber}.pdf`);
-};
+const forEachPage = (pageNumbers, cb) => Promise.all(pageNumbers.map(cb));
+const getPdfPath = (selectedFile, pageNumber) =>  selectedFile.replace('PN_A.pdf', `PN_A.${pageNumber}.pdf`);
 
 const pdftk = (command) => {
   return new Promise((resolve, reject) => {
@@ -55,10 +54,6 @@ const pdftk = (command) => {
   });
 };
 
-const forEachPage = (pageNumbers, cb) => {
-  return Promise.all(Array.from(Array(pageNumbers)).map((x, i) => { return cb(i + 1); }));
-};
-
 ipcMain.on('send-pages', (event, args) => {
   const sendClientAlert = (cbArg) => { event.sender.send('send-pages-alert', cbArg); };
   const fn = args.confirmed ? sendPages : checkPages;
@@ -66,40 +61,39 @@ ipcMain.on('send-pages', (event, args) => {
 });
 
 const checkPages = (args, sendClientAlert) => {
-  const { selectedFile } = args;
-  const slashSplit = selectedFile.split('/');
-  const dotSplit = slashSplit[slashSplit.length - 1].split('.');
-  const dateSerialized = dotSplit[0];
-
-  if (selectedFile.endsWith('.PN_A.pdf')) {
-    sendClientAlert({ dateSerialized, needsConfirmation: true, selectedFile, taskName: `${dateSerialized}: PDF exists, ready to send pages?` });
-  } else {
-    sendClientAlert({ taskName: `Invalid file: ${selectedFile}`, status: 'fail' });
-  }
+  const checker = new Checker(args.selectedFile, sendClientAlert);
+  return checker.check().catch(console.log);
 };
 
-class Sender {
-  constructor(dateSerialized, selectedFile, sendClientAlert) {
-    this.dateSerialized = dateSerialized;
+class Checker {
+  constructor(selectedFile, sendClientAlert) {
     this.selectedFile = selectedFile;
     this.sendClientAlert = sendClientAlert;
-    this.pageNumbers = 0;
   }
 
-  send() {
-    return this._getPageNumbers()
-      .then(this._splitPages.bind(this))
-      .then(this._transferPages.bind(this))
-      .then(this._sendSuccessNotification.bind(this));
+  check() {
+    return this._checkFile().then(this._getPageNumbers.bind(this));
+  }
+
+  _checkFile() {
+    if (this.selectedFile.endsWith('.PN_A.pdf')) {
+      return Promise.resolve();
+    } else {
+      this.sendClientAlert({ taskName: `Invalid file: ${this.selectedFile}`, status: 'fail' });
+      return Promise.reject();
+    }
   }
 
   _getPageNumbers() {
-    this.sendClientAlert({ taskName: `Preparing to read PDF`, status: 'pending' });
     return pdftk(`${this.selectedFile} dump_data`).then((lines) => {
       var pageNumbers = lines.split("\n").filter((line) => { return line.includes('NumberOfPages'); });
       pageNumbers = Number(pageNumbers[0].split('NumberOfPages: ')[1]);
-      this.sendClientAlert({ taskName: `Preparing to read PDF`, status: 'success' });
-      this.pageNumbers = pageNumbers;
+
+      const slashSplit = this.selectedFile.split('/');
+      const dotSplit = slashSplit[slashSplit.length - 1].split('.');
+      const dateSerialized = dotSplit[0];
+
+      this.sendClientAlert({ dateSerialized, needsConfirmation: true, pageNumbers, selectedFile: this.selectedFile, taskName: 'Ready to send page(s)' });
 
       return Promise.resolve();
     }).catch((e) => {
@@ -108,19 +102,44 @@ class Sender {
       return Promise.reject();
     });
   };
+}
+
+const sendPages = (args, sendClientAlert) => {
+  const sender = new Sender(args.dateSerialized, args.selectedFile, args.pageNumbersToSend, sendClientAlert);
+  return sender.send().catch(console.log);
+};
+
+class Sender {
+  constructor(dateSerialized, selectedFile, pageNumbers, sendClientAlert) {
+    this.dateSerialized = dateSerialized;
+    this.selectedFile = selectedFile;
+    this.sendClientAlert = sendClientAlert;
+
+    if (Array.isArray(pageNumbers)) {
+      this.pageNumbers = pageNumbers;
+    } else {
+      this.pageNumbers = Array.from({ length: pageNumbers }, (v, k) => { return k + 1; });
+    }
+  }
+
+  send() {
+    return this._splitPages()
+      .then(this._transferPages.bind(this))
+      .then(this._sendSuccessNotification.bind(this));
+  }
 
   _splitPages() {
-    this.sendClientAlert({ taskName: `Preparing ${this.pageNumbers} pages`, status: 'pending' });
+    this.sendClientAlert({ taskName: `Preparing ${this.pageNumbers.length} page(s)`, status: 'pending' });
 
     return forEachPage(this.pageNumbers, (pageNumber) => {
       return pdftk(`${this.selectedFile} cat ${pageNumber}-${pageNumber} output ${getPdfPath(this.selectedFile, pageNumber)}`)
       .catch((e) => {
-        this.sendClientAlert({ taskName: `Preparing ${this.pageNumbers} pages`, status: 'fail' });
+        this.sendClientAlert({ taskName: `Preparing ${this.pageNumbers.length} page(s)`, status: 'fail' });
         this.sendClientAlert({ taskName: `ERROR: ${e}`, status: 'fail' });
         return Promise.reject();
       });
     }).then(() => {
-      this.sendClientAlert({ taskName: `Preparing ${this.pageNumbers} pages`, status: 'success' });
+      this.sendClientAlert({ taskName: `Preparing ${this.pageNumbers.length} page(s)`, status: 'success' });
       return Promise.resolve();
     });
   };
@@ -141,13 +160,7 @@ class Sender {
           const originFilename = getPdfPath(this.selectedFile, pageNumber);
           var destFilename = originFilename.split("/");
           destFilename = destFilename[destFilename.length - 1];
-          conn.put(originFilename, destFilename, (err) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve();
-            }
-          });
+          conn.put(originFilename, `files/${destFilename}`, (err) => { err ? reject(err) : resolve(); });
         });
       });
     }).then(() => {
@@ -158,29 +171,24 @@ class Sender {
       });
     }).catch((e) => {
       this.sendClientAlert({ taskName: `ERROR: ${e}`, status: 'fail' });
-      this.sendClientAlert({ taskName: `Sending ${this.pageNumbers} pages to the printer`, status: 'fail' });
+      this.sendClientAlert({ taskName: `Sending ${this.pageNumbers.length} page(s) to the printer`, status: 'fail' });
       return Promise.reject();
     });
   };
 
   _sendSuccessNotification() {
-    this.sendClientAlert({ taskName: `Sending ${this.pageNumbers} pages to the printer`, status: 'success' });
+    this.sendClientAlert({ taskName: `Sending ${this.pageNumbers.length} page(s) to the printer`, status: 'success' });
 
     return request({
       method: 'POST',
       uri: config.slack_settings.webhook_url,
       form: {
-        payload: JSON.stringify(Object.assign({}, { text: `Pages sent for ${this.dateSerialized}` }, config.slack_settings))
+        payload: JSON.stringify(Object.assign({}, { text: `Sent ${this.pageNumbers.length } page(s) for ${this.dateSerialized}` }, config.slack_settings))
       }
     })
     .then(() => {
-      this.sendClientAlert({ taskName: 'SUCCESS', status: 'success' });
+      this.sendClientAlert({ taskName: `Success: Sent page(s) ${this.pageNumbers.join(', ')}`, status: 'success' });
       return Promise.resolve();
     });
   };
 }
-
-const sendPages = (args, sendClientAlert) => {
-  const sender = new Sender(args.dateSerialized, args.selectedFile, sendClientAlert);
-  return sender.send().catch(console.log);
-};
--- renderer.js
+++ renderer.js
@@ -7,48 +7,129 @@ const statusCssMap = {
   'success': 'success'
 };
 
-const enableSendButton = () => document.getElementById("sendButton").disabled = false;
+const enableSelectPDFButton = () => document.getElementById("selectPDFButton").disabled = false;
 const addSendStatus = (location, text) => document.getElementById('sendStatus').insertAdjacentHTML(location, text);
 
 const getConfirmation = (args) => {
-  const { dateSerialized, taskName } = args;
+  const { dateSerialized, pageNumbers, taskName } = args;
 
-  const confirmed = confirm(taskName);
-  if (confirmed) {
-    addSendStatus('afterbegin', `<h4 class="center">Run Log for ${dateSerialized}:</h4>`);
-    ipcRenderer.send('send-pages', Object.assign({}, { confirmed: true }, args));
-  } else {
-    enableSendButton();
-  }
+  const confirmHTML = `
+    <div id="sendFormInner">
+      <hr><div class="card-text"><h5>Select pages for ${dateSerialized}:</h5></div>
+
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-check">
+            <label class="form-check-label">
+              <input type="radio" class="form-check-input" id="sendAllPages">All pages (1 thru ${pageNumbers})
+            </label>
+          </div>
+          <div class="form-check">
+            <label class="form-check-label">
+              <input type="radio" class="form-check-input" id="sendSpecificPages">Specific pages
+            </label>
+          </div>
+          <div id="sendSpecificPagesSelectContainer"></div>
+        </div>
+
+        <div class="col-md-6">
+          <button type="button" class="btn btn-danger" id="sendButton" disabled>SEND</button>
+        </div>
+      </div>
+
+      <hr><div id="sendStatus"></div>
+    </div>
+  `;
+
+  const sendPages = (args, type) => {
+    const { dateSerialized, pageNumbers, taskName } = args;
+
+    let specificPageNumbers;
+    if (document.getElementById('sendSpecificPages').checked) {
+      specificPageNumbers = $("select#sendSpecificPagesSelect").val().map(Number);
+    }
+    const pageNumbersToSend = specificPageNumbers || pageNumbers;
+
+    let task;
+    if (type === 'full') {
+      task = `${taskName} 1 thru ${pageNumbers} for ${dateSerialized}?`;
+    } else {
+      task = `${taskName} ${specificPageNumbers.join(', ')} for ${dateSerialized}?`;
+    }
+
+    if (confirm(task)) {
+      addSendStatus('afterbegin', `<h5 class="center">Run Log for ${dateSerialized}:</h5>`);
+      ipcRenderer.send('send-pages', Object.assign({}, { confirmed: true, pageNumbersToSend }, args));
+
+      document.getElementById('sendAllPages').disabled = true;
+      document.getElementById('sendSpecificPages').disabled = true;
+      document.getElementById('sendButton').disabled = true;
+    } else {
+      enableSelectPDFButton();
+    }
+  };
+
+  document.getElementById('sendForm').insertAdjacentHTML('beforeend', confirmHTML);
+
+  let editionType;
+  document.getElementById('sendButton').addEventListener('click', (e) => {
+    e.preventDefault();
+    sendPages(args, editionType);
+  });
+
+  document.getElementById('sendAllPages').addEventListener('click', (e) => {
+    if (e.target.checked) {
+      document.getElementById('sendButton').disabled = false;
+      editionType = 'full';
+    }
+  });
+
+  document.getElementById('sendSpecificPages').addEventListener('click', (e) => {
+    if (e.target.checked) {
+      document.getElementById('sendSpecificPagesSelectContainer').insertAdjacentHTML(
+        'afterbegin',
+        `<br><select id="sendSpecificPagesSelect" multiple="multiple" style="width: 100%"></select>`
+      );
+
+      const pageNumbersOptions = Array.from(Array(pageNumbers)).map((x, i) => { return { id: (i + 1), text: (i + 1) }; });
+      $('select#sendSpecificPagesSelect').select2({ data: pageNumbersOptions });
+
+      document.getElementById('sendButton').disabled = false;
+      editionType = 'specific';
+    }
+  });
 };
 
-ipcRenderer.on('send-pages-alert', (event, args) => {
-  if (args.needsConfirmation) return getConfirmation(args);
+const initializeListener = () => {
+  ipcRenderer.on('send-pages-alert', (event, args) => {
+    if (args.needsConfirmation) return getConfirmation(args);
 
-  const { status, taskName } = args;
-  const taskId = taskName.split(/[^A-Za-z0-9]/).join("_");
-  const element = document.querySelector(`#sendStatus > #${taskId}`);
-  const cssClass = `alert alert-${statusCssMap[status]}`;
+    const { status, taskName } = args;
+    const taskId = taskName.split(/[^A-Za-z0-9]/).join("_");
+    const element = document.querySelector(`#sendStatus > #${taskId}`);
+    const cssClass = `alert alert-${statusCssMap[status]}`;
 
-  if (element) {
-    element.className = cssClass;
-  } else {
-    addSendStatus('beforeend', `<div class="${cssClass}" role="alert" id="${taskId}">${taskName}</div>`);
-  }
+    if (element) {
+      element.className = cssClass;
+    } else {
+      addSendStatus('beforeend', `<div class="${cssClass}" role="alert" id="${taskId}">${taskName}</div>`);
+    }
 
-  if ((taskName === 'SUCCESS' && status === 'success') || (status === 'fail')) {
-    enableSendButton();
-    addSendStatus('beforeend', `<div class="alert alert-info" role="alert"><a class="alert-link" href="${printerPagesLink}">Check printer pages</a></div>`);
-  }
-});
+    if (taskName.includes('Success:') && status === 'success') {
+      enableSelectPDFButton();
+      addSendStatus('beforeend', `<div class="alert alert-info" role="alert"><a class="alert-link" href="${printerPagesLink}">Check printer pages</a></div>`);
+    }
+  });
+};
 
 const initializePage = () => {
-  document.getElementById("sendButton").addEventListener("click", (e) => {
-    document.getElementById("sendStatus").innerHTML = '';
-
+  document.getElementById("selectPDFButton").addEventListener("click", (e) => {
     dialog.showOpenDialog({ properties: ['openFile'] }, (selectedFiles) => {
-      e.toElement.disabled = true;
-      ipcRenderer.send('send-pages', { confirmed: false, selectedFile: selectedFiles[0] });
+      if (selectedFiles) {
+        e.toElement.disabled = true;
+        if ($('div#sendFormInner')) $('div#sendFormInner').html('');
+        ipcRenderer.send('send-pages', { confirmed: false, selectedFile: selectedFiles[0] });
+      }
     });
   });
 
@@ -60,4 +141,7 @@ const initializePage = () => {
   });
 };
 
-initializePage();
+$(document).ready(() => {
+  initializeListener();
+  initializePage();
+});
